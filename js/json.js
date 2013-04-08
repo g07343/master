5afa8ca5641d3587dc3a2f4fa2f102be027261f8
Matthew Lewis <Matt@unknown20c9d0b44cf1.gateway.2wire.net>
2013-04-08 12:15:55 -0500

    Initial commit of VFW project files
    Commit of VFW project files to be used in the MIU course

---
+++ js/json.js
@@ -0,0 +1,35 @@
+//Matthew Lewis
+//VFW Project 4
+//Term: 1303
+
+
+
+var json = {
+	"character1": {
+		"name": ["Name: ", "Tim the Terrible"],
+		"gender": ["Gender: ", "Male"],
+		"age":    ["Age: ", "42"],
+		"origin": ["From: ", "Black-Mountains"],
+		"role":   ["Class: ", "barbarian"],
+		"level":  ["Level: ", "9"],
+		"bio":    ["Bio: ", "A terrifying barbarian!"]
+	},
+	"character2": {
+		"name": ["Name: ", "Dan the Dangerous"],
+		"gender": ["Gender: ", "Male"],
+		"age":    ["Age: ", "37"],
+		"origin": ["From: ", "Oc'tyre-Desert"],
+		"role":   ["Class: ", "rogue"],
+		"level":  ["Level: ", "7"],
+		"bio":    ["Bio: ", "A master thief."]
+	},
+	"character3": {
+		"name": ["Name: ", "Killer Kim"],
+		"gender": ["Gender: ", "Female"],
+		"age":    ["Age: ", "34"],
+		"origin": ["From: ", "Jergain-Highlands"],
+		"role":   ["Class: ", "mage"],
+		"level":  ["Level: ", "5"],
+		"bio":    ["Bio: ", "An unparalleled spellcaster."]
+	}	
+}
